dafe842f4ad3c21c08f320f480ad569f5382ab1e
internet007@nate.com <internet007@nate.com>
2021-06-15 09:34:20 +0900

    js이론 추가파일

---
+++ ch04function/js/event.js
@@ -0,0 +1,10 @@
+function showTxt() {
+    var text = document.getElementById("desc");
+    text.style.display = "block";
+}
+
+function hideTxt() {
+    var text = document.getElementById("desc");
+    text.style.display = "none";
+
+}
